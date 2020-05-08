43f88ba7bf95ab4f30b3594629f02613db9a26cb
Shuntaro Takahashi <st@Shuntaros-MacBook-Pro.local>
2020-05-08 17:26:29 +0900

    add language.py

---
+++ stylefact/finance.py
@@ -0,0 +1,21 @@
+"""
+pass
+"""
+
+def tail_distribution(x):
+    pass
+
+def distribution(x):
+    pass
+
+def acf(x):
+    pass
+
+def leverage_effect(x):
+    pass
+
+def gainloss_asymmetry(x):
+    pass
+
+def coarsefine_volatility(x):
+    pass
---
+++ stylefact/language.py
@@ -0,0 +1,17 @@
+"""
+The toolkit for the statistical laws of natural languages
+"""
+from collections import Counter
+
+def zipf(words):
+    pass
+
+def Heap(words):
+    pass
+
+def MI(words):
+    """
+    Mutual Information
+    Mutual Information Functions of Natural Language Texts. Wentian Li. Santa Fe Research 1989.
+    """
+    pass
