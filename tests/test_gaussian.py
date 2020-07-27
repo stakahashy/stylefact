570e5dbf7651954e2450c8d5f8a2485113094110
stakahashi <shushakura@gmail.com>
2020-07-27 18:51:49 +0900

    add tests

---
+++ tests/test_gaussian.py
@@ -0,0 +1,18 @@
+import numpy as np
+import sys
+import pytest
+import stylefact.finance as sff
+
+
+def test_prob():
+    assert 1 == 1
+    pass
+
+def test_correlation():
+    pass
+
+def test_leverage_effect():
+    pass
+
+def test_gainloss_asymmetry():
+    pass
---
+++ tests/test_random.py
@@ -0,0 +1,16 @@
+import pytest
+import stylefact.language as sfl
+
+
+def test_Zipf():
+    assert 1 == 1
+    pass
+
+def test_Heap():
+    pass
+
+def test_Ebeling_Neiman():
+    pass
+
+def test_MI():
+    pass
